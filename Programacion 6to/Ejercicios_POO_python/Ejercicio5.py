d8d9dec57e99b3487d8077cde8ebcc7bcf1cd415
LautaroVelez <lautivelez28@gmail.com>
2022-04-29 12:10:06 -0300

    ejercicio5

---
+++ Programacion 6to/Ejercicios_POO_python/Ejercicio5.py
@@ -0,0 +1,13 @@
+class Persona:
+    def persona(self,nom,edad):
+        self.nom=input("Ingrese su nombre:")
+        self.edad=input("Ingrese su edad:")
+    
+class Empleado(self,nom,edad,salario):
+    self.salario=salario
+
+    def pagar_impuestos(self):
+        if self.salario>=3000:
+            print("El empleado debe pagar impuestos")
+        else:
+            print("El empleado no debe pagar impuestos")
