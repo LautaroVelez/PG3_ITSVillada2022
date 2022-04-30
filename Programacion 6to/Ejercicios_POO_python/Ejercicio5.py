8cc476fc682679ac30655a87e2016ad0e8cd50d2
LautaroVelez <lautivelez28@gmail.com>
2022-04-30 15:35:17 -0300

    ejercico5

--- Programacion 6to/Ejercicios_POO_python/Ejercicio5.py
+++ Programacion 6to/Ejercicios_POO_python/Ejercicio5.py
@@ -1,13 +1,30 @@
 class Persona:
-    def persona(self,nom,edad):
-        self.nom=input("Ingrese su nombre:")
-        self.edad=input("Ingrese su edad:")
+    def persona(self):
+        self.nom=str(input("Ingrese su nombre:"))
+        self.edad=int(input("Ingrese su edad:"))
+
+    def imprimir(self):
+        print("Nombre:",self.nom)
+        print("Edad:",self.edad)
     
-class Empleado(self,nom,edad,salario):
-    self.salario=salario
+class Empleado(Persona):
+    def empleado(self):
+        super().persona()
+        self.salario=int(input("Ingrese su salario:"))
+
+    def imprimir(self):
+        super().imprimir()
+        print("Salario:",self.salario)
 
     def pagar_impuestos(self):
         if self.salario>=3000:
             print("El empleado debe pagar impuestos")
         else:
             print("El empleado no debe pagar impuestos")
+
+
+persona1=Empleado()
+persona1.empleado()
+persona1.imprimir()
+persona1.pagar_impuestos()
+
