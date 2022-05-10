4d88b2be63c8e19bbdaf46ab090639c9bdd79a33
LautaroVelez <lautivelez28@gmail.com>
2022-05-10 02:22:45 -0300

    ej3 terminado

--- Programacion 6to/exceptions_python/ej.3.py
+++ Programacion 6to/exceptions_python/ej.3.py
@@ -1,7 +1,14 @@
+meses=("enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre")
 
 try:
-    meses=("enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre")
     input=int(input("Ingrese su numero de mes"))
+    
+    if input<13 and input>0:
+        print("Su mes es:",meses[input-1])
+    else:
+        print("Introduce un numero valido wn")
 
 except IndexError:
-    pass
+    print("Introduzca un numero valido")
+except ValueError:
+    print("Introduzca un numero wn")
