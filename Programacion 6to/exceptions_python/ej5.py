8abec69501879b2409f0a92907005a3a56f34303
LautaroVelez <lautivelez28@gmail.com>
2022-05-12 09:00:07 -0300

    ej5 garca

--- Programacion 6to/exceptions_python/ej5.py
+++ Programacion 6to/exceptions_python/ej5.py
@@ -1,12 +1,19 @@
-string= "holadoszetafour"
+string= str(input("Ingrese una cadena: "))
 
 try:
-    file1 = open("Lauu\Programacion\PG3_ITSVillada2022\Programacion 6to\exceptions_python\string1.txt", "w") 
-    file1.write(string)
-    file1.close()
- 
-    f = open('Original.txt', 'r')
-    if f.mode=='r':
-        contents= f.read()
+    esono = any(chr.isdigit() for chr in string)
+    if esono == True:
+        print("No ponga numeros porfas")
+        
+    elif esono== False:
+        file1 = open(".\PG3_ITSVillada2022\Programacion 6to\exceptions_python\string1.txt", "w") 
+        file1.write(string)
+        file1.close()
+
+        f = open('Original.txt', 'r')
+
+        if f.mode=='r':
+            contents= f.read() 
+    
 except:
     pass
